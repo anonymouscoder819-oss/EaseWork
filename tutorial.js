f4fad10e51c8e42e1722bfbe5ae024e4611f94df
anonymouscoder819-oss <anonymouscoder819@gmail.com>
2026-01-15 17:40:30 +0530

    some small changes

--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,31 @@
-function greet(name){
-    console.log("Hi " + name + " Have a nice day!!")
-}
+// let a = [1, 93, 5, 6, 88];
+
+// a.forEach((value,index, arr) => {
+//     console.log(value, index, arr);
+// });
+
+// let new_a = a.map((e)=> {return e**2})
+// console.log(new_a)
+
+
+// let a = [1, 63, 25, 4, 3]
+
+// let new_a = a.map((e) => {
+//     return e ** 2
+// });
+
+// console.log(new_a)
+
+// let arr = [1,5,8,13,17,2,9,18,7];
+
+// const greaterThanSeven = (e) =>{
+//     if (e>7){
+//         return true;
+//     }
+//     return false;
+// };
+
+// console.log(arr.filter(greaterThanSeven));
+// arr.sort((a, b) => b-a)
+// console.log(arr);
 
